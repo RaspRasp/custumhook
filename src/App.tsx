66ebd5b8d71d46f075e2cb6dbee80132eb200742
RaspRasp <108247612+RaspRasp@users.noreply.github.com>
2022-07-01 17:40:21 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
+const user = {
+  id: 1,
+  name: "Rio",
+  email: "test@mail.com",
+  address: "日本"
+};
+export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city} ${user.address.suite} ${user.address.street}`
+        }));
+        setUserProfiles(data);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+      <UserCard user={user} />
+    </div>
+  );
+}
