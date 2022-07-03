49f596090096acd2c125574a6a24f06907b8e0b3
RaspRasp <108247612+RaspRasp@users.noreply.github.com>
2022-07-03 19:02:30 +0900

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,27 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import { User } from "./types/api/user";
-import { useState } from "react";
-import { UserProfile } from "./types/userProfile";
-const user = {
-  id: 1,
-  name: "Rio",
-  email: "test@mail.com",
-  address: "日本"
-};
+import { useAllUsers } from "./hooks/useAllUsers";
+
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const onClickFetchUser = () => {
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city} ${user.address.suite} ${user.address.street}`
-        }));
-        setUserProfiles(data);
-      });
-  };
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchUser = () => getUsers();
 
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
-      <UserCard user={user} />
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
